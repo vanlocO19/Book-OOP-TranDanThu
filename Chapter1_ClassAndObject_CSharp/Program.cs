93ab7585964c44aa49889c3c7a8c93718333739a
vanloc1808 <vanloc1808@users.noreply.github.com>
2021-09-27 09:58:09 +0700

    chapter 1 C#

---
+++ Chapter1_ClassAndObject_CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] argv)
+    {
+        Circle mycir = new Circle();
+        double r = 10;
+
+        mycir.set(r);
+
+        Console.WriteLine("R = " + r);
+        Console.WriteLine("Area = " + mycir.area());
+        Console.WriteLine("Perimeter = " + mycir.perimeter());
+        Console.ReadLine();
+    }
+}
